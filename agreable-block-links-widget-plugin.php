ddd130ddb3f02cb95262355540e2d65164b1fed6
Leo <leonoragilmour@sky.com>
2017-06-15 11:29:14 +0100

    init

---
+++ agreable-block-links-widget-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: Agreable Block Links Widget Plugin
+* Plugin URI: http://github.com/shortlist-digital/agreable-block-links-widget-plugin
+* Description: A plugin for the block links widget
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class AgreableBlockLinksWidgetPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new AgreableBlockLinksWidgetPlugin();
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Agreable Block Links Widget Plugin
+A plugin for the block links widget
